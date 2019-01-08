49b39a112527bf5bf9ef9375d1434473d7648890
Nicolas Girardot <nicolas.girardot@neofacto.com>
2019-01-08 14:37:22 +0100

    Removed writing to file

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,6 @@
 # This won't be executed if keys already exist (i.e. from a volume)
 ssh-keygen -A
 
-# Copy authorized keys from ENV variable
-echo $AUTHORIZED_KEYS  >>$AUTHORIZED_KEYS_FILE
-
 # Chown data folder (if mounted as a volume for the first time)
 chown data:data /home/data
 
